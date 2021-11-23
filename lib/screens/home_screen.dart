659abd513b8c4df76989ecdc8f03e42192e32d54
engida32 <engida32lishan@gmail.com>
2021-11-23 20:40:44 +0300

    🔥 home screen updated

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import "package:flutter/material.dart";
+import 'package:moviesapp/style/theme.dart' as Style;
+import 'package:moviesapp/widget/genre.dart';
+import 'package:moviesapp/widget/now_playing.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Style.Colors.mainColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Style.Colors.mainColor,
+        centerTitle: true,
+        leading: Icon(EvaIcons.menu2Outline, color: Colors.white),
+        title: Text("Movie App"),
+        actions: [IconButton(onPressed: null, icon: Icon(EvaIcons.searchOutline, color: Colors.white))],
+      ),
+      body: ListView(
+        children: [
+          NowPlaying(),
+          GenreScreen()
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/style/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  Colors();
+  static const Color mainColor = Color(0xff151c26);
+  static const Color secondaryColor = Color(0xfff4c10f);
+  static const Color titleColor = Color(0xff5a606b);
+  
+}
