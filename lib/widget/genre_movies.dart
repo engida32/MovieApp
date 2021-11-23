5cc5f020ce436590c8b20f8379d0ff67915c4a58
engida32 <engida32lishan@gmail.com>
2021-11-23 23:37:31 +0300

    ✅ fetching movies from api based on their category with new widget

---
+++ lib/widget/genre_movies.dart
@@ -0,0 +1,188 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:moviesapp/bloc/get_movies_byGenre_bloc.dart';
+import 'package:moviesapp/model/movie.dart';
+import 'package:moviesapp/model/movie_response.dart';
+import 'package:moviesapp/style/theme.dart' as Style;
+
+class GenreMovies extends StatefulWidget {
+  final int genreId;
+  const GenreMovies({Key? key, required this.genreId}) : super(key: key);
+
+  @override
+  _GenreMoviesState createState() => _GenreMoviesState(genreId);
+}
+
+class _GenreMoviesState extends State<GenreMovies> {
+   final int genreId ;
+  _GenreMoviesState(this.genreId);
+  @override
+  void initState() {
+    super.initState();
+    // MoviesListByGenreBloc..getMoviesByGenre(genreId);
+    movieByGenreBloc..getMoviesByGenre(genreId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<MovieResponse>(
+        stream: movieByGenreBloc.subject.stream,
+        builder: (context, AsyncSnapshot<MovieResponse> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data?.error != null &&
+                snapshot.data!.error.length > 0) {
+              return _buildErrorWidgets(snapshot.data?.error);
+            }
+            return buildMoviesByGenreWidget(snapshot.data!);
+          } else if (snapshot.hasError) {
+            return _buildErrorWidgets(snapshot.error);
+          } else {
+            return _buildLoadingWidget();
+          }
+        });
+  }
+
+  Widget _buildLoadingWidget() {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 25.0,
+          width: 25.0,
+          child: CircularProgressIndicator(
+            valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+            strokeWidth: 4.0,
+          ),
+        )
+      ],
+    ));
+  }
+
+  
+
+  Widget _buildErrorWidgets(error) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Error Occured ',
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.red.shade50)),
+        ],
+      ),
+    );
+  }
+  Widget buildMoviesByGenreWidget(MovieResponse data) {
+    List<Movie> movies = data.movies;
+    if (movies.length == 0) {
+      return Container(
+        child: Text(" No Movies ",style: TextStyle(color: Colors.white,fontSize: 13),),
+      );
+    } else {
+      return Container(
+        height: 270,
+        padding: EdgeInsets.only(left: 10),
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.only(
+                  top: 10,
+                  left: 10,
+                  right: 10,
+                ),
+                child: Column(
+                  children: [
+                    movies[index].poster == null
+                        ? Container(
+                            height: 180,
+                            width: 120,
+                            decoration: BoxDecoration(
+                                color: Style.Colors.secondaryColor,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2)),
+                                shape: BoxShape.rectangle),
+                            child: Column(
+                              children: [
+                                Icon(EvaIcons.filmOutline,
+                                    color: Colors.white, size: 50)
+                              ],
+                            ),
+                          )
+                        : Container(
+                            width: 180,
+                            height: 120,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://image.tmdb.org/t/p/w200/" +
+                                          movies[index].poster),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 180,
+                      child: Text(
+                        movies[index].title,
+                        maxLines: 2,
+                        style: TextStyle(
+                            height: 1.5,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          movies[index].rating.toString(),textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            inherit: true,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                          width: 12,
+                        ),
+                        RatingBar.builder(
+                          itemSize: 8,
+                          initialRating: movies[index].rating / 2,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 3),
+                          wrapAlignment: WrapAlignment.start,
+                          itemBuilder: (context, _) => Icon(EvaIcons.star,
+                              color: Style.Colors.secondaryColor,size: 12,),
+                          glowColor: Colors.amber,
+                          onRatingUpdate: (rating) {
+                            print(rating);
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }),
+      );
+    }
+  }
+}
