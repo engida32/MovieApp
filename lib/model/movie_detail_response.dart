0866662bb3cee4e4a002b5b4d2241699942f19ab
engida32 <engida32lishan@gmail.com>
2021-11-24 21:46:10 +0300

    ✨ added movie detail model

---
+++ lib/model/movie_detail_response.dart
@@ -0,0 +1,14 @@
+import 'package:moviesapp/model/movie_detail.dart';
+
+class MovieDetailResponse {
+  final MovieDetail movieDetail;
+  final String error;
+
+  MovieDetailResponse(this.movieDetail, this.error);
+  MovieDetailResponse.fromJson(Map<String, dynamic> json)
+      : movieDetail = MovieDetail.fromJson(json),
+        error = '';
+  MovieDetailResponse.withError(String errorValue)
+      : movieDetail = MovieDetail(null,null,null,[],"",null),
+        error = errorValue;
+}
