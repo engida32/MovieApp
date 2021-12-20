fbab7e2d76c6ae6107fcff3bbcb8453eab371e68
engida32 <engida32lishan@gmail.com>
2021-12-20 21:05:06 +0300

    🎨  movie casts added

--- lib/model/cast_response.dart
+++ lib/model/cast_response.dart
@@ -7,7 +7,7 @@ class CastResponse {
   CastResponse(this.casts, this.error);
   CastResponse.fromJson(Map<String, dynamic> json)
       : casts =
-            (json['casts'] as List).map((e) => new Cast.fromJson(e)).toList(),
+            (json['cast'] as List).map((e) => new Cast.fromJson(e)).toList(),
         error = '';
   CastResponse.withError(String errorValue)
   :casts =[],
